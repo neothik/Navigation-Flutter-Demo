696bac0564fbaa5f7ae2c50b07cb01f76c284652
Karthik <gkarthik24@gmail.com>
2020-08-26 11:25:51 +0530

    Final commit

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_demo_starter/screen2.dart';
+import 'screen2.dart';
 
 class Screen1 extends StatelessWidget {
   @override
@@ -12,7 +14,11 @@ class Screen1 extends StatelessWidget {
         child: RaisedButton(
           color: Colors.red,
           child: Text('Go Forwards To Screen 2'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return Screen2();
+            }));
+          },
         ),
       ),
     );
